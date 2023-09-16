0cab395bb6894485b7e159e339a3a066307ccd40
Terracota1 <gianlucadelarosa@gmail.com>
2023-09-15 23:57:56 -0600

    Cambios Generales
    Se realizaron cambios generales

---
+++ TowerDefense/Assets/_Scripts/ZombiePolicia.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ZombiePolicia : MonoBehaviour
+{
+    public GameObject objetivo;
+    public int vida = 50;
+
+    public Animator Anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<NavMeshAgent>().SetDestination(objetivo.transform.position);
+        Anim = GetComponent<Animator>();
+        Anim.SetBool("IsMoving", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Objetivo")
+        {
+            Anim.SetBool("IsMoving", false);
+            Anim.SetTrigger("OnObjetivoReached");
+        }
+    }
+
+    public void Danar()
+    {
+        objetivo?.GetComponent<Objetivo>().RecibirDano(40);
+    }
+
+    public void RecibirDano(int dano = 5)
+    {
+        vida -= dano;
+    }
+}
