804fbc1943ac8caaa30306d29a56e5178fc7371c
Terracota1 <gianlucadelarosa@gmail.com>
2023-09-16 14:04:49 -0600

    Enemigos
    Cambios a los enemigos

--- TowerDefense/Assets/_Scripts/AdministradorUI.cs
+++ TowerDefense/Assets/_Scripts/AdministradorUI.cs
@@ -1,18 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AdministradorUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject canvasPrincipal;
+    public GameObject menuPerdiste;
+    public SpawnearEnemigos referenciaSpawner;
+    public Objetivo referenciaObjetivo;
+
+    private void OnEnable()
     {
-        
+        referenciaObjetivo.EnObjetivoDestruido += MostrarMenuGameOver;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        referenciaObjetivo.EnObjetivoDestruido -= MostrarMenuGameOver;
+    }
+
+    public void MostrarMenuFinOleada()
+    {
+
+    }
+
+    public void OcultarMenuFinOleada()
+    {
+
+    }
+
+    public void MostrarMenuGameOver()
+    {
+        menuPerdiste.SetActive(false);
+    }
+
+    public void OcultarMenuGameOver()
+    {
+        menuPerdiste.SetActive(false);
+    }
+
+    public void FinalizarJuego()
+    {
+        Application.Quit();
+    }
+
+    public void CargarMenuPrincipal()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ReintentarNivel()
+    {
+        int escenaActual = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(escenaActual);
     }
 }
