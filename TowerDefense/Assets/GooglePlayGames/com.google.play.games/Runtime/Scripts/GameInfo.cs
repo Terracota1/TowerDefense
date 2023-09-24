15e498e06a260f3c7281b57ac223efd9ba19d5da
Terracota1 <gianlucadelarosa@gmail.com>
2023-09-24 01:28:20 -0600

    Cambios
    Cambios

--- TowerDefense/Assets/GooglePlayGames/com.google.play.games/Runtime/Scripts/GameInfo.cs
+++ TowerDefense/Assets/GooglePlayGames/com.google.play.games/Runtime/Scripts/GameInfo.cs
@@ -36,9 +36,9 @@ namespace GooglePlayGames {
         private const string UnescapedWebClientId = "WEB_CLIENTID";
         private const string UnescapedNearbyServiceId = "NEARBY_SERVICE_ID";
 
-        public const string ApplicationId = ""; // Filled in automatically
+        public const string ApplicationId = "725176572165"; // Filled in automatically
         public const string IosClientId = "__IOS_CLIENTID__"; // Filled in automatically
-        public const string WebClientId = ""; // Filled in automatically
+        public const string WebClientId = "725176572165-b9u29ue2g8f91vud0rfg87du2hg0j4di.apps.googleusercontent.com"; // Filled in automatically
         public const string NearbyConnectionServiceId = "";
 
         public static bool ApplicationIdInitialized() {
